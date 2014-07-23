d1935634231f5c3447b7d7f7000e54f8164339bc
Ivan Yunda <ivan.yunda@gmail.com>
2014-07-23 09:32:38 +0300

    Made most part of the graph

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 ;(function (d3, w, d){
 
     var height = 300,
-        barWidth = 30,
+        barWidth = 40,
         main = d3.select('#main'),
         graph = d3.select('#graph'),
+        scale = d3.select('#scale').append('svg').attr({ 'height': height + 20, 'width': 50 }),
         videoSelect = d3.select('#video_select'),
         svg = graph.append('svg').attr('height', height + 20),
         months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -12,48 +13,292 @@
     // retrieve data
     d3.json('./data.json', init);
 
-    function buildGraph (dataHash){
+    function getMaxInt (value){
+        var digitsLength = value.toString().length,
+            zerosNumber = digitsLength - 1,
+            testNumber = '1',
+            max = 0,
+            residue;
+
+        if ( digitsLength <= 1 ){
+            max = 10;
+            return max;
+        }
+
+        while ( zerosNumber ) {
+            testNumber += '0';
+            zerosNumber--;
+        }
+        testNumber = +testNumber;
+
+        residue = value % testNumber;
+        rounded = value + (testNumber - residue);
+
+        // max = rounded + testNumber;
+
+        // console.log(testNumber);
+
+        if ( value < 100 ){
+            rounded = rounded.toString().split('');
+            rounded[0] = +rounded[0] > 5 ? '10': '5';
+            max = +(rounded.join(''));
+        } else {
+            max = rounded;
+        }
+        
+        return max;
+    }
+
+    function buildGraph (dataHash, daily){
         // get an array of key-value objects
         var data = d3.map(dataHash).entries(),
-            max = d3.max(data, function (item){
-                return item.value;
-            }),
-            y = d3.scale.linear().domain([0, max]).range([height, 0]),
+            monthsData = [];
+
+        // parse date strings
+        data.forEach(function (item){
+            item.key = new Date(item.key);
+        });
 
         // sort data by date
         data = data.sort(function (a, b){
-            var aTime = (new Date(a.key)).getTime(),
-                bTime = (new Date(b.key)).getTime();
+            var aTime = a.key.getTime(),
+                bTime = b.key.getTime();
             return aTime - bTime;
         });
 
+        if (daily ) {
+            // make daily values
+            data = data.map(function (item, i){
+                var result = { key: item.key };
+
+                if ( i > 0 ){
+                    result.value = item.value - data[i - 1].value;
+                } else {
+                    result.value = item.value;
+                }
+                return result;
+            });
+        }
+
+        // fill months data
+        data.forEach((function (){
+            var currentMonth = '';
+            return function (item, i){
+                // use d3.time.format("%b %Y").parse;
+                var month = months[item.key.getMonth()] + ' ' + item.key.getFullYear();
+
+                if ( month !== currentMonth ) {
+                    monthsData.push({
+                        text: month,
+                        margin: i * barWidth
+                    });
+
+                    console.log(i * barWidth);
+                    
+                    currentMonth = month;
+                }
+            }
+        })());
+
+        var max = d3.max(data, function (item){
+                return item.value;
+            }),
+            y = d3.scale.linear().domain([0, getMaxInt(max)]).range([height, 0]),
+            scaleData = y.ticks(4).map(y.tickFormat(4, "d")),
+            graphWidth = data.length * barWidth;
+
         // set the canvas width
-        svg.attr('width', data.length * barWidth);
+        svg.attr('width', graphWidth);
 
-        svg.selectAll('g').remove();
+        // clear canvas
+        svg.selectAll('*').remove();
 
-        var bar = svg.selectAll('g')
+        svg.append('line')
+            .attr({
+                'x1': 0,
+                'y1': height,
+                'x2': graphWidth,
+                'y2': height,
+                'class': 'timeline'
+            });
+
+        // draw month ticks
+        var monthTick = svg.selectAll('g.month_tick')
+            .data(monthsData)
+            .enter()
+            .append('g')
+            .attr({
+                'transform': function(d, i) { console.log(d.margin); return 'translate(' + d.margin + ', 0)'; },
+                'class': 'month_tick'
+            });
+        
+        monthTick.append('line')
+            .attr({
+                'x1': 0,
+                'y1': 0,
+                'x2': 0,
+                'y2': height
+            });
+
+        monthTick.append('text')
+            .attr({
+                'x': 10,
+                'y': 32
+            })
+            .text(function (d){ return d.text });
+
+        // draw scale
+        scale.select('g').remove();
+
+        scale.append('g')
+            .attr('class', 'axis')
+            .call(d3.svg.axis()
+                .scale(y)
+                .orient('right')
+                .ticks(4)
+                .tickFormat( d3.format('s') ) );
+
+
+
+        // draw bars
+
+        if ( daily ){
+            drawDailyBars(data, y);
+        } else {
+            drawGrossBars(data, y);
+        }
+        
+        
+    }
+
+    function drawDailyBars (data, y) {
+        var bar = svg.selectAll('g.bar')
             .data(data)
             .enter()
             .append('g')
-            .attr('transform', function(d, i) { return 'translate(' + i * barWidth + ', 0)'; });
+            .attr({
+                'transform': function(d, i) { return 'translate(' + i * barWidth + ', 0)'; },
+                'class': 'bar'
+            });
 
-        bar.append('line')
+
+        bar.append('rect')
             .attr({
-                'stroke': '#39ba82',
-                'stroke-width': '3'
-            })
-            .attr({
-                'x1': 0,
-                'y1': function (d){
-                    return Math.round(y(d.value));
+                'shape-rendering': 'crispEdges',
+                'fill': 'rgba(57, 186, 130, 0.3)',
+                'width': barWidth,
+                'height': function (d){
+                    return height - y(d.value);
                 },
-                'x2': barWidth,
-                'y2': function (d){
+                'transform': function (d, i) {
+                    return 'translate(0,'+ y(d.value) + ')'; 
+                }
+            });
+
+        bar.append('rect')
+            .attr({
+                'shape-rendering': 'crispEdges',
+                'fill': 'rgba(57, 186, 130, 1)',
+                'width': barWidth,
+                'height': 1,
+                'transform': function (d, i) {
+                    return 'translate(0,'+ y(d.value) + ')'; 
+                }
+            });
+
+        bar.append('text')
+            .attr({
+                'class': 'value',
+                'x': barWidth / 2,
+                'y': function (d){
+                    return y(d.value) - 5;
+                }
+            })
+            .text(function(d) { return d.value; });
+
+        drawTimeline(bar);
+    }
+
+    function drawGrossBars (data, y){
+        var area = d3.svg.area()
+            .interpolate('monotone')
+            .x(function(d, i) { return i * barWidth + (barWidth / 2); })
+            .y0(height)
+            .y1(function(d, i) { return y(d.value); });
+
+        svg.append('path')
+            .datum(data)
+            .attr({
+                'class': 'area',
+                'd': area
+            });
+
+
+        var line = d3.svg.line()
+            .x(function(d, i) { return i * barWidth + (barWidth / 2); })
+            .y(function(d, i) { return y(d.value); })
+            .interpolate('linear');
+
+        svg.append('path')
+            .datum(data)
+            .attr({
+                'class': 'curve',
+                'd': line
+            });
+
+        var bar = svg.selectAll('g.bar')
+            .data(data)
+            .enter()
+            .append('g')
+            .attr({
+                'transform': function(d, i) { return 'translate(' + i * barWidth + ', 0)'; },
+                'class': 'bar'
+            });
+
+        bar.append('circle')
+            .attr({
+                'class': 'circle_stroke',
+                'r': 7,
+                'cx': barWidth / 2,
+                'cy': function (d){
                     return Math.round(y(d.value));
                 }
             });
 
+        bar.append('circle')
+            .attr({
+                'class': 'circle',
+                'r': 5,
+                'cx': barWidth / 2,
+                'cy': function (d){
+                    return Math.round(y(d.value));
+                }
+            });
+
+        drawTimeline(bar);
+        
+    }
+
+    function drawTimeline (bar){
+        // days 
+        bar.append('text')
+            .attr({
+                'x': barWidth / 2,
+                'y': height + 15,
+                'class': 'date'
+            })
+            .text( function(d){ 
+                return d.key.getDate();
+            });
+
+        bar.append('line')
+            .attr({
+                'x1': barWidth,
+                'y1': height,
+                'x2': barWidth,
+                'y2': height + 5,
+                'class': 'timeline'
+            });
     }
 
 
@@ -74,7 +319,7 @@
         videoSelect.on('change', function (e){
             var videoIndex = +this.value;
 
-            buildGraph(data[videoIndex].views);
+            buildGraph(data[videoIndex].views, true);
         });
 
         buildGraph(data[0].views);
